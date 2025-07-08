2108280a839975e30cdd1e0907799794a1e973d5
sumon0002001 <sumon0002001@gmail.com>
2025-07-09 01:50:58 +0600

    Added header

---
+++ components/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-10 supports-[backdrop-filter]:bg-background/60">Header</header>
+  )
+}
+
+export default Header
